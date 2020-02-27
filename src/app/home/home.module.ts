daec087934005d4b3901096f61f6fa14be9524b1
Luiz Renato <luiz.santos@logusinfo.com.br>
2020-02-27 18:25:22 -0300

    a-6 Criação componente de mensagens de validação

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { SigninComponent } from './signin/sigin.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [SigninComponent],
     imports : [
         CommonModule,
-        ReactiveFormsModule]
+        ReactiveFormsModule,
+        VMessageModule
+    ]
 })
 export class HomeModule {
 }
---
+++ src/app/shared/components/vmessage/vmessage.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    templateUrl : './vmessage.component.html',
+    selector : 'ap-vmessage'
+})
+export class VMessageComponent {
+
+    @Input()
+    text :string='';
+}
